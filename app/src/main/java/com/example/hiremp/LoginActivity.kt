be358b651703e6ccf196c5f115a2351d9e65c690
Varun Sharma <imva508@gmail.com>
2019-02-21 00:29:59 +0530

    Done creating selection screen

---
+++ app/src/main/java/com/example/hiremp/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.hiremp
+
+import android.os.Bundle
+import android.app.Activity
+
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+}
