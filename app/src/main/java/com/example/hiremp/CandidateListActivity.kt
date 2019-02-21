e139c3f5dea9206538dd7d0d54333cd6476f504c
Varun Sharma <imva508@gmail.com>
2019-02-21 22:41:10 +0530

    Created the candidate fragment

---
+++ app/src/main/java/com/example/hiremp/CandidateListActivity.kt
@@ -0,0 +1,15 @@
+package com.example.hiremp
+
+import android.os.Bundle
+import android.app.Activity
+
+import kotlinx.android.synthetic.main.activity_candidate_list.*
+
+class CandidateListActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_candidate_list)
+    }
+
+}
